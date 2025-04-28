f1036a0e1ec1d241092807bc346e163476cb66ec
C1Forever <sz4021@nyu.edu>
2025-04-28 13:44:26 -0400

    build main method

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class TicTacToe {
-    
+    private static Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        int mode = 0;
+
+        // Read which mode to play with
+        do {
+            System.out.println("Enter 1 for 2 players, 2 for playing against the CPU, or 0 to quit.");
+            mode = input.nextInt();
+
+            switch (mode) {
+                case 1:
+                    twoPlayers();
+                    break;
+                case 2:
+                    againstCPU();
+                    break;
+                case 0:
+                    System.out.println("Thanks for playing!");
+                    break;
+                default:
+                    System.out.println("Invalid input.");
+            }
+        } while (true);
+
+        input.close();
+    }
 }
