07cb1c73a39cc66c1630afb438c69829902099f6
CRYSTAL_CM\ynott <gdcrystal04@gmail.com>
2025-04-28 18:47:50 -0400

    the cpu can pick random moves (it cannot detect easy wins/losses yet)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
     // a 3x3 board
@@ -6,9 +6,9 @@ public class TicTacToe {
     // 2 -> occupied by player2
     // 0 -> unoccupied
     private static int[][] board = new int[3][3];
-    private static int consecutiveError = 0;
-    private static int[] numError = {0, 0};
-    private static Scanner input = new Scanner(System.in);
+    private static int consecutiveError = 0;                //number of same inputs
+    private static int[] numError = {0, 0};                 //number of invalid inputs
+    private static Scanner input = new Scanner(System.in);  
 
     public static void main(String[] args) {
         int mode = 0;
@@ -24,8 +24,8 @@ public class TicTacToe {
                     twoPlayers();
                     break;
                 case 2:
-                    //againstCPU();
-                    System.out.println("Implementing...");
+                    againstCPU();
+                    //System.out.println("Implementing...");
                     break;
                 case 0:
                     System.out.println("Thanks for playing!");
@@ -92,6 +92,148 @@ public class TicTacToe {
                 }
             }
         }
+    }
+
+    public static void againstCPU(){
+
+        //player 1 is you, player 2 is cpu
+        int currentPlayer = 1;
+
+        //cpu will not play selected positions (start from 1~9)
+        ArrayList<Integer> openPos = new ArrayList<>();
+        for (int i = 1; i <= 9; i ++){
+            openPos.add(i);
+        }
+
+        initBoard();
+        boolean error = false;
+        int position;
+
+        while (true) {
+
+            //continue game if theres no error
+            if (!error) {
+                
+                //display board once
+                if (currentPlayer == 1){
+                    displayBoard();
+                }
+            }
+
+            //your turn
+            if (currentPlayer == 1){
+                System.out.print("Player 1's turn: ");
+                position = input.nextInt();
+            }
+
+            //cpu turn
+            else {
+                boolean clearWin = false;
+
+                //MAKE SURE IT DOESNT RANDOMIZE WHEN THERES A CLEAR WIN
+                if (clearWin){
+                    position = 0; //placeholder
+                }
+
+                else {
+                    
+                    //random number 1~9, +1 to avoid cpu from choosing 0
+                    Random rand = new Random();
+                    int randPos = 0;
+                    boolean valid = false;
+
+                    //randomize until cpu chooses valid position
+                    while (!valid){
+                        randPos = rand.nextInt(9) + 1;
+
+                        //go throught cpu's available moves
+                        for (int i = 0; i < openPos.size(); i ++){
+
+                            //choose this available space and remove it
+                            if (openPos.get(i) == randPos){
+                                openPos.remove(i);
+                                valid = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    position = randPos;
+                    //System.out.println("cpu picked " + position);
+                }
+            }
+            
+
+            //process input
+            if (position == 0) {
+                System.out.println("Player 1 forfeits the game.");
+                System.out.println("Game Over! CPU wins!");
+                return;
+            }
+
+            if (isValid(position)) {
+                error = false;
+                consecutiveError = 0;
+                changeBoard(currentPlayer, position);
+
+                //this position is selected, remove it from cpu's option
+                openPos.remove(Integer.valueOf(position));
+
+                // Check if the game ends
+                if (checkWin(currentPlayer)) {
+                    displayBoard();
+                    
+                    if (currentPlayer == 1){
+                        System.out.println("Game Over! Player " + currentPlayer + " wins!");
+                    }
+                    else {
+                        System.out.println("Game Over! CPU wins!");
+                    }
+                    
+                    return;
+                }
+                
+                else if (checkTie()) {
+                    displayBoard();
+                    System.out.println("Game Over! It's a draw!");
+                    return;
+                }
+                
+                else {
+                    // switch player
+                    currentPlayer = (currentPlayer == 1 ? 2 : 1);
+                }
+            }
+            
+            else {
+                System.out.println("Incorrect entry, please try again.");
+                error = true;
+
+                //ASK ABOUT THIS LATER
+                consecutiveError++;
+                numError[currentPlayer-1]++;
+
+                // 'and' OR 'or'???
+                if (consecutiveError >= 3 || numError[currentPlayer-1] >= 5) {
+                    System.out.println("Player " + currentPlayer + " forfeit the game due to reaching maximum incorrect entries.");
+                    int winner = (currentPlayer == 1 ? 2 : 1);
+
+                    //this will always display "CPU wins" but its here
+                    //just in case the CPU bugs and chooses invalid positions
+                    if (currentPlayer == 1){
+                        //dumb error handling
+                        System.out.println("Game Over! Player " + currentPlayer + " wins!");
+                    }
+                    else {
+                        System.out.println("Game Over! CPU wins!");
+                    }
+
+                    return;
+                }
+            }
+        }
+
+
     }
     
     public static boolean isValid(int position) {
